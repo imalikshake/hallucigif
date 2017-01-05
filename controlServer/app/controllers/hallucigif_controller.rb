a6771f049132f9ad24567b65d3aee04c8d8759b2
Ubuntu <ubuntu@ip-172-31-36-147.eu-west-1.compute.internal>
2017-01-05 20:49:41 +0000

    try get calling bash stuff working

--- controlServer/app/controllers/hallucigif_controller.rb
+++ controlServer/app/controllers/hallucigif_controller.rb
@@ -6,7 +6,8 @@ class HallucigifController < ApplicationController
   end
 
   def render_gif
-    @job = Delayed::Job.enqueue RenderJob.new
+    puts "ayyyyyyy" + @url.to_s
+    @job = Delayed::Job.enqueue RenderJob.new(url: @url, start: @start, duration: @duration)
   end
   
   def submit
@@ -15,6 +16,11 @@ class HallucigifController < ApplicationController
     @second = params[:video_start]["from_time(6i)"]
     @gif_duration  = params[:duration]["duration_id"]
     @time = "00:" + @minute + ":" + @second
+    @url = @ytlink
+    @start = @time
+    @duration = "00:00:0" + @gif_duration
+    
+    puts "ayye" + @url
 
     job = Hallucigif.new 
     job.link = @ytlink
--- controlServer/app/jobs/render_job.rb
+++ controlServer/app/jobs/render_job.rb
@@ -1,17 +1,26 @@
 class RenderJob < ProgressJob::Base
 
-def initialize
-  puts "inside init"
+attr_accessor :url, :start, :duration
+
+def initialize(url, start, duration)
+  @url = url.to_s
+  @start = start.to_s
+  @duration = duration.to_s
+  puts "initialised " + self.url
 end
 
 def perform
-  puts "inside job"
   update_stage('Exporting users')    
-  update_progress_max(100)
-  (1..100).each do |i|
-    update_progress(step: 10)
-    puts "inside loop"
-  end
+  update_progress_max(10)
+  #`cd /home/ubuntu/; ./processvid.sh "https://www.youtube.com/watch?v=zO_1f_olvCQ" "00:30:30" "00:00:05" 10`
+  #`cd /home/ubuntu/; ./processvid.sh #{self.url}  #{self.start} #{self.duration} #{self.id}`
+  puts self.url
+  puts %x(cd /home/ubuntu/; echo #{@url}  #{@start} #{@duration} 0)
+  update_progress(step: 10)
+  #(1..10).each do |i|
+  #  update_progress(step: 1)
+  #  sleep(1)
+  #end
 end
 
 end
